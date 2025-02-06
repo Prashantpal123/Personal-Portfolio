e94f93110388e375061cdc53adb5d606ffe1f48a
Prashant Pal <142970947+Prashantpal123@users.noreply.github.com>
2025-02-06 12:22:11 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsK2qIWSSKfiswJflDVYkZVRqu9oP7tWQ",
+  authDomain: "portfolio-48d55.firebaseapp.com",
+  projectId: "portfolio-48d55",
+  storageBucket: "portfolio-48d55.firebasestorage.app",
+  messagingSenderId: "153703765281",
+  appId: "1:153703765281:web:995cf209c16b3cf867da60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
